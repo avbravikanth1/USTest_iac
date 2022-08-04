5c81b84e838b32224f1bd1f9e2db9e070f711406
Rajasekhar Gurugubelli <raja.gurugubelli@DRITS.COM>
2022-08-03 18:44:53 -0700

    added apache2Install.sh script

---
+++ scripts/apache2Install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Checking the apache2 status"
+sudo systemctl status apache2 1 >> /dev/null
+if [ $? -gt 0 ]; then
+   echo " Updating the apt repository"
+   sudo apt -y update
+   echo "Installing Apache2"
+   sudo apt install -y apache2 1 >> /dev/null
+   sudo systemctl start apache2
+   sudo systemctl enable apache2
+else
+   echo " Apcahe2 is already installed"
+fi
