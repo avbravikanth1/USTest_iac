6710a00dad4a666242da3ae3c0292746918abbd6
rajasekhar2304 <raajasekharr@gmail.com>
2022-08-04 02:01:09 -0700

    modified the script

--- scripts/apache2Install.sh
+++ scripts/apache2Install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "Checking the apache2 status"
-sudo systemctl status apache2 1 >> /dev/null
+sudo systemctl status apache2 1> /dev/null
 if [ $? -gt 0 ]; then
    echo " Updating the apt repository"
-   sudo apt -y update
+   sudo apt -y update 1> /dev/null
    echo "Installing Apache2"
-   sudo apt install -y apache2 1 >> /dev/null
-   sudo systemctl start apache2
+   sudo apt install -y apache2 1:> /dev/null
    sudo systemctl enable apache2
 else
    echo " Apache2 is already installed"
