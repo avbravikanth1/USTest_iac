324dd4e0e2ee0fbed3f2bbff13d3b6ce08dd858c
rajasekhar2304 <raajasekharr@gmail.com>
2022-08-05 01:50:27 -0700

    changes made in script

--- scripts/apache2Install.sh
+++ scripts/apache2Install.sh
@@ -2,25 +2,28 @@
 echo "Checking the apache2 status"
 sudo systemctl status apache2 >/dev/null 2>&1
 if [ $? -gt 0 ]; then
-   echo "Updating the apt repository"
+   echo "-------------------------------------------------"
+   echo "Apache2 is not found. Preparing to install"
+   echo "---Updating the apt repository---"
    sudo apt -y update >/dev/null 2>&1
-   echo "Installing Apache2"
+   echo "--Installing Apache2---"
    sudo apt install -y apache2 >/dev/null 2>&1
-   echo "Enabling apache2"
+   echo "---Enabling apache2---"
    sudo systemctl enable apache2 >/dev/null 2>&1
    echo "---------------------------------------------------------------------------"
    echo "Apache2 installation completed and moving to next step to enable modules"
    echo "---------------------------------------------------------------------------"
-   echo "Enabling Apache Header module"
+   echo "---Enabling Apache Header module---"
    sudo a2enmod headers >/dev/null 2>&1
-   echo "Enabling proxy http module"
+   echo "---Enabling proxy http module---"
    sudo a2enmod proxy_http >/dev/null 2>&1
    sudo systemctl restart apache2
    echo "Modules enabling is completed"
+   echo "------------------------------------------------------------------"
 else
-   echo " Apache2 is already installed"
+   echo "Apache2 is already installed"
 fi
-echo "Checking node version"
+echo "---Checking node version---"
 sudo node -v >/dev/null 2>&1
 if [ $? -gt 0 ]; then
    echo "-------------------------------------------------"
